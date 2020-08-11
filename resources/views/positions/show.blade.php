86d01ccd1b017b2a2b97a8b8b3e1dd71f844478c
nale988 <65071690+nale988@users.noreply.github.com>
2020-08-11 07:27:29 +0200

    Update show.blade.php

--- resources/views/positions/show.blade.php
+++ resources/views/positions/show.blade.php
@@ -459,67 +459,69 @@
 </div>
 
 @if($position -> devicetype -> id == 3)
-<br />
-<br />
-<div class="card">
-    <div class="card-header">
-        Dodaj radne sate za kompresor
+    @if($user -> id == 1)
+    <br />
+    <br />
+    <div class="card">
+        <div class="card-header">
+            Dodaj radne sate za kompresor
+        </div>
+        <div class="card-body">
+        <form class="m-2" method="post" action="{{ route('storeworkinghours') }}">
+            @csrf
+            <input type="hidden" name="position_id" value="{{ $position -> id }}" />
+            <div class="row">
+                <div class="col-4">
+                    <div class="form-group">
+                        @if(!empty($lastworkinghours->total))
+                        <input type="text" name="total" class="form-control" placeholder="{{ $lastworkinghours -> total }}h" />
+                        @else
+                        <input type="text" name="total" class="form-control" placeholder="Radni sati" />
+                        @endif
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="form-group">
+                        @if(!empty($lastworkinghours->loaded))
+                            <input type="text" name="loaded" class="form-control" placeholder="{{ $lastworkinghours -> loaded}}h" />
+                        @else
+                            <input type="text" name="loaded" class="form-control" placeholder="Opterećeni sati" />
+                        @endif
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="form-group">
+                        @if(!empty($lastworkinghours->starts))
+                            <input type="text" name="starts" class="form-control" placeholder="{{ $lastworkinghours -> starts }}" />
+                        @else
+                            <input type="text" name="starts" class="form-control" placeholder="Startovi motora" />
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <div class="form-group">
+                        <label for="description">Komentar:</label>
+                        <textarea class="form-control" name="description" rows="1"></textarea>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="form-group">
+                        <label for="date">Datum mjerenja:</label>
+                        <input type="date" name="date" class="form-control" value="{{ now()->format('Y-m-d') }}" />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-dark btn-sm">Sačuvaj radne sate</button>
+                </div>
+            </div>
+        </form>
+        </div>
     </div>
-    <div class="card-body">
-    <form class="m-2" method="post" action="{{ route('storeworkinghours') }}">
-        @csrf
-        <input type="hidden" name="position_id" value="{{ $position -> id }}" />
-        <div class="row">
-            <div class="col-4">
-                <div class="form-group">
-                    @if(!empty($lastworkinghours->total))
-                    <input type="text" name="total" class="form-control" placeholder="{{ $lastworkinghours -> total }}h" />
-                    @else
-                    <input type="text" name="total" class="form-control" placeholder="Radni sati" />
-                    @endif
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="form-group">
-                    @if(!empty($lastworkinghours->loaded))
-                        <input type="text" name="loaded" class="form-control" placeholder="{{ $lastworkinghours -> loaded}}h" />
-                    @else
-                        <input type="text" name="loaded" class="form-control" placeholder="Opterećeni sati" />
-                    @endif
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="form-group">
-                    @if(!empty($lastworkinghours->starts))
-                        <input type="text" name="starts" class="form-control" placeholder="{{ $lastworkinghours -> starts }}" />
-                    @else
-                        <input type="text" name="starts" class="form-control" placeholder="Startovi motora" />
-                    @endif
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-8">
-                <div class="form-group">
-                    <label for="description">Komentar:</label>
-                    <textarea class="form-control" name="description" rows="1"></textarea>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="form-group">
-                    <label for="date">Datum mjerenja:</label>
-                    <input type="date" name="date" class="form-control" value="{{ now()->format('Y-m-d') }}" />
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <button type="submit" class="btn btn-dark btn-sm">Sačuvaj radne sate</button>
-            </div>
-        </div>
-    </form>
-    </div>
-</div>
+    @endif
 @endif
 
 <br />
