4d50ac886610a5d9ee5265c4af36b95ee2613686
nale988 <65071690+nale988@users.noreply.github.com>
2020-08-25 13:20:13 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,6 @@
         </div>
         <div class="card-body">
             @foreach($pareto as $position)
-            @if($position ->totalworkorders = 2)
-                @break;
-            @endif
             <div class="row">
                 <div class="col-1 text-right text-truncate">
                     <a href="{{ route('workorders', $position -> position)}}" style="color:#000000">
