3770a2ed4468196c0d13e82bf142c09879af2a5e
nale988 <65071690+nale988@users.noreply.github.com>
2020-09-02 09:59:20 +0200

    Update sitesettings.php

--- config/sitesettings.php
+++ config/sitesettings.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'navision_date' => '24. 8. 2020.',
-    'workorders_date' => '4. 8. 2020.',
+    'workorders_date' => '24. 8. 2020.',
     'pareto' => 15,
     'timezoneoffset' => '+2 hour',
 ];
